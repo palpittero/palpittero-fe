1ff0486bb63007b5f14da44a086f66ecb34c7b60
Pablo Veiga <vcpablo@gmail.com>
2022-09-23 00:42:03 -0300

    chore: configure CI/CD

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import viteCompression from 'vite-plugin-compression'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+console.log(process.env)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
